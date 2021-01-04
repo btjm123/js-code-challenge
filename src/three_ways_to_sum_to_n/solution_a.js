94637c10e2b1bea2aebf8bdf1236917580429806
btjm123 <btjm123@gmail.com>
2021-01-04 16:54:42 +0800

    Update solution_a.js

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -3,6 +3,11 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
+// simple for-loop that loops from 1 to N.
 var sum_to_n = function(n) {
-    // your code here
+  var ans=0;
+  for (var i=1;i<=n;i++) {
+    ans += i;
+  }
+  return ans;
 };
